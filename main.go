a7f7a4a12aaa87a5350574810f06563432911262
Elvis O <elvis.osujic@gmail.com>
2023-05-16 07:19:29 +0100

    changes

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"time"
+
+	"github.com/bigveezus/my-cli/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeClient: pokeClient,
+	}
+	startRepl(cfg)
 }
