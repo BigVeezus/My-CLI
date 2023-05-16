3e17293056869f0ac0e74d64cca3a426311b6bb8
Elvis O <elvis.osujic@gmail.com>
2023-05-16 10:32:33 +0100

    Added caching

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"time"
+
 	"github.com/bigveezus/my-cli/internal/pokeapi"
 )
 
 func main() {
-	pokeapiClient := pokeapi.NewClient()
+	pokeapiClient := pokeapi.NewClient(5 * time.Second, time.Minute * 5)
 	cfg := &config{
 		pokeapiClient: pokeapiClient,
 	}
